c1293ea65fc61f415bfef3b8b65b56f9e8f20f29
Himanshu Panwar <hpanwar875@gmail.com>
2026-06-21 19:40:17 +0530

    add  prisma file for using single sourcetruth

---
+++ ride-service/src/lib/prisma.ts
@@ -0,0 +1,43 @@
+import "dotenv/config"
+import { PrismaClient } from "../generated/prisma/client.js"
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const { Pool } = pg;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+}
+
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error("Database_URL environment is not set");
+}
+
+const adapter = new PrismaPg({
+  connectionString,
+  max: 20,
+  idleTimeoutMillis: 60000,
+  connectionTimeoutMillis: 30000
+})
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({ adapter });
+} else {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient({ 
+      adapter,
+      log: ['query', 'error', 'warn'], 
+    });
+  }
+  prisma = globalForPrisma.prisma;
+}
+
+process.on('beforeExit', async () => {
+  await prisma.$disconnect();
+});
+
+export { prisma }
